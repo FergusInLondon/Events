b245be2bae5e546cbd33a279c17ff0652aca33f8
Fergus Morrow <fergus@fergus-morrow.com>
2016-05-18 07:15:27 +0100

    Adhere to PSR2. Monitor adherance via CI

--- src/Handler.php
+++ src/Handler.php
@@ -9,57 +9,66 @@
  * file that was distributed with this source code.
  */
 
-class Handler {
+class Handler
+{
 
-	/** @var FergusInLondon\Events\Registry */
-	private $registry;
+    /**
+     * @var FergusInLondon\Events\Registry
+     */
+    private $registry;
 
-	/** @var string */
-	private $registryIdentifier;
-	
+    /**
+     * @var string
+     */
+    private $registryIdentifier;
+    
 
-	/**
-	 * The constructor requires a Callable object, this is the actual event
-	 *  handler. Note, you may override this, in conjunction with the "respond"
-	 *  method, to avoid manually calling (+ binding) the closure.
-	 *
-	 * @param Callable  $action      The Handling action, responsible for responding to an event.
-	 */
-	public function __construct(Callable $action){
-		$this->action = $action->bindTo($this, $this);
-	}
-	
+    /**
+     * The constructor requires a Callable object, this is the actual event
+     *  handler. Note, you may override this, in conjunction with the "respond"
+     *  method, to avoid manually calling (+ binding) the closure.
+     *
+     * @param Callable $action The Handling action, responsible for responding to an event.
+     */
+    public function __construct(callable $action)
+    {
+        $this->action = $action->bindTo($this, $this);
+    }
+    
 
-	/**
-	 * When an event occurs, this is the method which get calls. This is simply
-	 *  a wrapper function around our Callable by default.
-	 *
-	 * @param array     $params      Parameters to be passed to the handling Callable, given to the event when it was triggered.
-	 */
-	public function respond($params = array()){
-		return call_user_func_array($this->action, $params);
-	}
-	
+    /**
+     * When an event occurs, this is the method which get calls. This is simply
+     *  a wrapper function around our Callable by default.
+     *
+     * @param array $params Parameters to be passed to the handling Callable, given to the event when it was triggered.
+     */
+    public function respond($params = array())
+    {
+        return call_user_func_array($this->action, $params);
+    }
+    
 
-	/**
-	 * Upon registering a Handler with a Registry, this method gets called
-	 *  by the Registry - and passes with it the Registry object and an
-	 *  alphanumeric string which identifies the Handler. These two variables
-	 *  can be used to subsequently cancel the Handler.
-	 *
-	 * @param Registry  $registry    Registry object the Handler has registered with.
-	 * @param string    $identifier  An alphanumeric identifier, which allows the Registry to locate this Handler.
-	 */
-	public function associateRegistry(Registry $registry, $identifier){
-		$this->registry = $registry;
-		$this->registryIdentifier = $identifier;
-	}
-	
+    /**
+     * Upon registering a Handler with a Registry, this method gets called
+     *  by the Registry - and passes with it the Registry object and an
+     *  alphanumeric string which identifies the Handler. These two variables
+     *  can be used to subsequently cancel the Handler.
+     *
+     * @param Registry $registry   Registry object the Handler has registered with.
+     * @param string   $identifier An alphanumeric identifier, which allows the Registry to locate this Handler.
+     */
+    public function associateRegistry(Registry $registry, $identifier)
+    {
+        $this->registry = $registry;
+        $this->registryIdentifier = $identifier;
+    }
+    
 
-	/**
-	 * Remove this Handler from the Registry.
-	 */
-	public function stopListening(){
-		$this->registry->removeHandler($this->registryIdentifier);
-	}
+    /**
+     * Remove this Handler from the Registry.
+     */
+    public function stopListening()
+    {
+        $this->registry->removeHandler($this->registryIdentifier);
+    }
 }
--- src/Registry.php
+++ src/Registry.php
@@ -9,75 +9,82 @@
  * file that was distributed with this source code.
  */
 
-class Registry {
-	
-	/** @var FergusInLondon\Events\Handler[] */
-	private $handlers = array();
+class Registry
+{
+    
+    /**
+     * @var FergusInLondon\Events\Handler[]
+     */
+    private $handlers = array();
 
 
-	/**
-	 * Registers a new handler for an event based upon an identifying
-	 *  string. 
-	 *
-	 * @param  string   $ident              Alphanumeric identifier for the event.
-	 * @param  Handler  $listener           Handler object to respond to events.
-	 *
-	 * @return self
-	 */
-	public function registerHandler($ident, Handler $listener){
-		if (!isset($this->handlers[$ident]) || !is_array($this->handlers[$ident])) {
-			$this->handlers[$ident] = array();
-		}
-		
-		
-		$count = array_push($this->handlers[$ident], $listener);
-		$listener->associateRegistry($this, sprintf("%s_%d", $ident, $count-1));
-		return $this;
-	}
+    /**
+     * Registers a new handler for an event based upon an identifying
+     *  string.
+     *
+     * @param string  $ident    Alphanumeric identifier for the event.
+     * @param Handler $listener Handler object to respond to events.
+     *
+     * @return self
+     */
+    public function registerHandler($ident, Handler $listener)
+    {
+        if (!isset($this->handlers[$ident]) || !is_array($this->handlers[$ident])) {
+            $this->handlers[$ident] = array();
+        }
+        
+        
+        $count = array_push($this->handlers[$ident], $listener);
+        $listener->associateRegistry($this, sprintf("%s_%d", $ident, $count-1));
+        return $this;
+    }
 
-	
-	/**
-	 * Triggers an event based upon the identifying string for that
-	 *  event. Optionally accepts an array of parameters to pass on
-	 *  to the handling method.
-	 *
-	 * @param  string   $ident              Alphanumeric identifier for the event.
-	 * @param  mixed[]  $params             Optional: parameters to pass to the handler object.
-	 */
-	public function trigger($ident, $params = array()){
-		if (isset($this->handlers[$ident]) && is_array($this->handlers[$ident])) {
-			foreach ($this->handlers[$ident] as $l){
-				call_user_func_array([$l, "respond"], [$params]);
-			}
-		}
-	}
-
-
-	/**
-	 * Removes a handler, this should only be called from the Handler
-	 *  object as it depends upon an identifier which is provided to
-	 *  the Handler object and stored as a private attribute.
-	 *
-	 * @param  string   $handlerIdentifier  Alphanumeric identifier for handler to remove from the registry.
-	 */
-	public function removeHandler($handlerIdentifier){
-		$identParts = explode('_', $handlerIdentifier);
-		unset( $this->handlers[ $identParts[0] ][ $identParts[1] ] );
-	}
+    
+    /**
+     * Triggers an event based upon the identifying string for that
+     *  event. Optionally accepts an array of parameters to pass on
+     *  to the handling method.
+     *
+     * @param string  $ident  Alphanumeric identifier for the event.
+     * @param mixed[] $params Optional: parameters to pass to the handler object.
+     */
+    public function trigger($ident, $params = array())
+    {
+        if (isset($this->handlers[$ident]) && is_array($this->handlers[$ident])) {
+            foreach ($this->handlers[$ident] as $l) {
+                call_user_func_array([$l, "respond"], [$params]);
+            }
+        }
+    }
 
 
-	/**
-	 * Clears handlers for a given event if an identifier is provided,
-	 *  alternatively, clears all registered handlers if no identifier
-	 *  is available.
-	 *
-	 * @param  string   $ident              Alphanumeric identifier for the event to clear the handlers from.
-	 */
-	public function clearHandlers($ident = null){
-		if (!is_null($ident) && isset($this->handlers[$ident]) && is_array($this->handlers[$ident])) {
-			$this->handlers[$ident] = array();
-		} else if(is_null($ident)){			
-			$this->handlers = array();
-		}
-	}
+    /**
+     * Removes a handler, this should only be called from the Handler
+     *  object as it depends upon an identifier which is provided to
+     *  the Handler object and stored as a private attribute.
+     *
+     * @param string $handlerIdentifier Alphanumeric identifier for handler to remove from the registry.
+     */
+    public function removeHandler($handlerIdentifier)
+    {
+        $identParts = explode('_', $handlerIdentifier);
+        unset($this->handlers[ $identParts[0] ][ $identParts[1] ]);
+    }
+
+
+    /**
+     * Clears handlers for a given event if an identifier is provided,
+     *  alternatively, clears all registered handlers if no identifier
+     *  is available.
+     *
+     * @param string $ident Alphanumeric identifier for the event to clear the handlers from.
+     */
+    public function clearHandlers($ident = null)
+    {
+        if (!is_null($ident) && isset($this->handlers[$ident]) && is_array($this->handlers[$ident])) {
+            $this->handlers[$ident] = array();
+        } elseif (is_null($ident)) {
+            $this->handlers = array();
+        }
+    }
 }
